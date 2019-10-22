540e231e6106ef1ca46e02ead3a9033585303e92
Zhilin Dong <yinzhid@princeton.edu>
2019-10-22 19:48:36 -0400

    new tiny test files

---
+++ server/tiny_database.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+# as;dlfjas;ldkfjl;adkjf;laskdjf;lasdkfj
+
+#-----------------------------------------------------------------------
+# tiny_database.py
+# connecting our thing with the actual database...
+#-----------------------------------------------------------------------
+
+from sqlite3 import connect
+from sys import stderr
+from os import path
+from tiny_post import Post
+
+#-----------------------------------------------------------------------
+
+class Database:
+    
+    def __init__(self):
+        self._connection = None
+
+    # connect to the database **rename this. also: how to postgres?
+    def connect(self):      
+        DATABASE_NAME = 'freat.sqlite'
+        if not path.isfile(DATABASE_NAME):
+            raise Exception('Database connection failed')
+        self._connection = connect(DATABASE_NAME)
+                    
+    # disconnect from the database
+    def disconnect(self):
+        self._connection.close()
+
+    # append a new post to the database
+    def append(self, title):
+        cursor = self._connection.cursor() # the cursor
+        APPEND_STRING = 'INSERT INTO postings(title) VALUES(?);'
+        cursor.execute(APPEND_STRING, [title]) 
+
+    # return all the entries in the postings table
+    def get_all(self):
+        cursor = self._connection.cursor()
+        QUERY_STRING = \
+            'select title FROM postings'
+        cursor.execute(QUERY_STRING) 
+
+        titles = []
+        row = cursor.fetchone()
+        while row is not None:
+            titles.append(row[0])
+            row = cursor.fetchone()
+        cursor.close()
+        return(titles)
+
+    # search for a post based on author?
+    def search(self, title):
+        cursor = self._connection.cursor()
+
+        QUERY_STRING = \
+            'select title, price from postings ' + \
+            'where title like ?'
+        cursor.execute(QUERY_STRING, [title]) 
+        
+        titles = []
+        row = cursor.fetchone()
+        while row is not None:  
+            titles.append(row[0])
+            row = cursor.fetchone()
+        cursor.close()
+        return titles
+
+#-----------------------------------------------------------------------
+
+# For testing:
+
+if __name__ == '__main__':
+    database = Database()
+    database.connect()
+    database.append("Sandwich")
+    database.append("Oyster")
+    database.append("Eggplant")
+    database.append("salami")
+    database.append("italian")
+    books = database.get_all()
+    for book in books:
+        print(book)
+    database.disconnect()
