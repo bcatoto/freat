88735ca156b371ddbe2e5c4a6954215c8e26152a
bcatoto <biancacatoto999@gmail.com>
2019-10-31 12:31:14 -0400

    renamed client_react to client

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function NavBar(props) {
+  return (
+    <Navbar className="p-2 bg-primary" variant="dark" expand="lg">
+      <Navbar.Brand className="ml-2">Freat</Navbar.Brand>
+      <Button
+        className="mr-auto" variant="navbar"
+        onClick={props.handleNewPost}
+      >
+        New post
+      </Button>
+      <Button  href="#profile" variant="navbar">Account</Button>
+    </Navbar>
+  );
+}
---
+++ client/src/components/Post.js
@@ -0,0 +1,77 @@
+import React from "react";
+import Accordion from "react-bootstrap/Accordion"
+import Card from "react-bootstrap/Card"
+import Button from "react-bootstrap/Button"
+import Badge from "react-bootstrap/Badge"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+
+import berries from "./../assets/images/berries.png"
+
+export default class Post extends React.Component {
+  renderDietOptions() {
+    const info = [
+      {
+        "key": 0,
+        "name": "Vegetarian",
+        "color": "vegetarian",
+      },
+      {
+        "key": 1,
+        "name": "Vegan",
+        "color": "vegan",
+      },
+      {
+        "key": 2,
+        "name": "Kosher",
+        "color": "kosher",
+      },
+      {
+        "key": 3,
+        "name": "Halal",
+        "color": "halal",
+      },
+      {
+        "key": 4,
+        "name": "Gluten-Free",
+        "color": "gluten",
+      }
+    ];
+
+    const diets = this.props.post.diet.map(i => info[i]);
+    return diets.map(diet =>
+      <Badge pill key={diet.key} variant={diet.color}>{diet.name}</Badge>
+    );
+  }
+
+  render() {
+    return (
+      <Accordion.Toggle as={Card} eventKey={this.props.post.id}>
+        <Card.Header>
+          <Container fluid className="p-0">
+            <Row noGutters="true">
+              <Card.Title className="mr-auto">{this.props.post.title}</Card.Title>
+              <span className="post-time">15 minutes ago</span>
+            </Row>
+            <Row noGutters="true" className="mt-1">
+              <Button variant="location">
+                <i className="fas fa-map-marker-alt mr-1"></i>{this.props.post.room} {this.props.post.building}
+              </Button>
+            </Row>
+          </Container>
+        </Card.Header>
+        <Card.Img src={berries} />
+        <Accordion.Collapse eventKey={this.props.post.id}>
+          <Card.Body>
+            {this.props.post.description}
+            <br/>
+            <em>Feeds approximately: {this.props.post.feeds}</em>
+          </Card.Body>
+        </Accordion.Collapse>
+        <Card.Footer>
+          {this.renderDietOptions()}
+        </Card.Footer>
+      </Accordion.Toggle>
+    );
+  }
+}
