3fcfa768881bdd05d7c04bfc694c2b57dc82d94d
bcatoto <biancacatoto999@gmail.com>
2019-11-05 00:00:18 -0500

    fixed searchbar button

--- client/src/components/Searchbar.js
+++ client/src/components/Searchbar.js
@@ -5,7 +5,7 @@ import Form from 'react-bootstrap/Form'
 export default function Searchbar() {
   return (
     <Form inline className="p-3">
-      <Form.Control id="form-search" className="flex-grow-1" type="text" placeholder="Search for food..." />
+      <Form.Control id="form-search" type="text" placeholder="Search for food..." />
       <Button variant="search"><i className='fas fa-search'></i></Button>
     </Form>
   );
