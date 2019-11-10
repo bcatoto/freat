0b7731bdbe290441d01f5c333e9ac5b5df0c05f6
claired976 <yinzhid@princeton.edu>
2019-11-10 12:46:41 -0500

    cas file, small changes to app

--- server/src/config.py
+++ server/src/config.py
@@ -8,6 +8,7 @@ class Development(object):
     TESTING = False
     SECRET_KEY = 'something'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # suppresses uneccesary error message
 
 class Production(object):
     """
@@ -16,6 +17,7 @@ class Production(object):
     DEBUG = False
     TESTING = False
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # suppresses uneccesary error message
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
 
 app_config = {
