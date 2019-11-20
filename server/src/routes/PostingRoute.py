37a45e931096f979e9a4f37438ab50f3fd50fd79
claired976 <yinzhid@princeton.edu>
2019-11-20 11:16:27 -0500

    put

--- server/src/routes/PostingRoute.py
+++ server/src/routes/PostingRoute.py
@@ -83,7 +83,8 @@ def updatePost(postid):
   # return custom_response({'error': 'permission denied'}, 400)
 
   try:
-    data = posting_schema.load(req_data['post'], partial=True)
+    #data = posting_schema.load(req_data['post'], partial=True)
+    data = posting_schema.load(req_data['post'])
     post[0].update(data) # need post[0] b/c the PostingModel.get_one_post(postid) list/only way to get sqlalchemy to return an object
     data = posting_schema.dump(post)
     return custom_response(data, 200)
