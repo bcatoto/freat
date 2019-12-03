212b2d84d2ffe4476428614a1cd476bde42f580c
claired976 <yinzhid@princeton.edu>
2019-12-02 20:03:37 -0500

    updated authentication route

--- server/src/routes/AuthenticationRoute.py
+++ server/src/routes/AuthenticationRoute.py
@@ -1,7 +1,15 @@
 from flask import request, json, Response, Blueprint, jsonify
+from PostingRoute import custom_response
+from ..CASClient import CASClient
 
 
 authentication_api = Blueprint('authentication', __name__)
 
 @authentication_api.route('/authenticate', methods=['GET'])
+def getUsername():
+  """
+  Get CAS username
+  """
+  username = CASClient().authenticate().rstrip('\n') # removes ending newline
+  return custom_response({'netid': username}, 200)
 
