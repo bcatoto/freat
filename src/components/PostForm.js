ff70c2c86b501734856c37507c0dccbca283d860
bcatoto <biancacatoto999@gmail.com>
2019-12-06 14:16:49 -0500

    reordered assets

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -4,7 +4,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 
-import buildings from "./assets/buildings.json";
+import buildings from "../assets/buildings.json";
 
 export default class PostForm extends React.Component {
   constructor(props) {
