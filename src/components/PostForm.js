34f1ac232577cb5fa44e93593723432b16acb93d
bcatoto <biancacatoto999@gmail.com>
2019-12-06 15:52:28 -0500

    character count in postform

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -34,6 +34,7 @@ export default class PostForm extends React.Component {
       room: false,
       building: false,
       images: false,
+      desc: true,
       feeds: true
     };
 
@@ -122,7 +123,7 @@ export default class PostForm extends React.Component {
     this.props.handleClose();
     this.setState({
       post: this.cleanPost(),
-      valid:  this.initialValid,
+      valid: this.initialValid,
       validForm: false
     });
   }
@@ -153,10 +154,11 @@ export default class PostForm extends React.Component {
     valid.room = post.room.length > 0;
     valid.building = post.building !== "-- Select building --";
     valid.images = post.images.length > 0;
+    valid.desc = post.desc.length < 251;
     valid.feeds = post.feeds > 0;
 
     const validForm = valid.title && valid.room && valid.building &&
-      valid.images && valid.feeds;
+      valid.images && valid.desc && valid.feeds;
     this.setState({ validForm });
   }
 
@@ -263,6 +265,9 @@ export default class PostForm extends React.Component {
                 value={this.state.post.desc}
                 onChange={this.handleChange}
               />
+              <Form.Text className="text-muted mt-1">
+                {this.state.post.desc.length}/250 characters
+              </Form.Text>
             </Form.Group>
 
             <Form.Group controlId="input-diet">
