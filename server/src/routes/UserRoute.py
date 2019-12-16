56410b80600da84b7fb8eeb41ed55062356794a2
claired976 <yinzhid@princeton.edu>
2019-12-16 12:30:24 -0600

    update user route

--- server/src/routes/UserRoute.py
+++ server/src/routes/UserRoute.py
@@ -1,10 +1,12 @@
 from flask import request, json, Response, Blueprint, jsonify
 from ..CASClient import CASClient
 from ..models.UserModel import UserModel, UserSchema
+import requests
 
 
 user_api = Blueprint('user', __name__)
 user_schema = UserSchema()
+URL = 'https://freat.herokuapp.com/api/v1/posting/'
 
 @user_api.route('/getCurrentUser', methods=['GET'])
 def getUsername():
@@ -19,6 +21,39 @@ def getUsername():
       user.save()
   return custom_response({'netid': username}, 200)
 
+@user_api.route('/going/<string:netid>', methods=['POST'])
+def updateGoing(netid):
+  """
+  Update that this user has clicked going
+  """
+  postid = request.get_json()['postid'] # does this work?
+  user = UserModel.get_user_byNetId(netid)
+  data = user_schema.dump(user, many=True) # get all this user's data
+  if (len(data) == 0):
+    return custom_response({'error': 'user not found'}, 404)
+
+  try:
+    posts_going = data[0]['posts_going']
+    
+    if postid in posts_going:
+      # removals
+      posts_going.remove(postid)
+      new_url = URL + 'removeGoing/' + str(postid)
+      requests.post(url = new_url)
+
+    else:
+      # additions
+      posts_going.append(postid)
+      new_url = URL + 'addGoing/' + str(postid)
+      requests.post(url = new_url)
+
+    data = {'posts_going': posts_going} # the new postid array
+    user[0].update(data) 
+    data = user_schema.dump(user)
+    return custom_response(data, 201)
+
+  except Exception as err:
+    return custom_response({'message': err}, 400)
 
 @user_api.route('/addUserGoing/<string:netid>', methods=['POST'])
 def addGoing(netid):
