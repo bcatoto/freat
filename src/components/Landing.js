59132a53c8f6f9b5435b2ce96347f3938cd10145
bcatoto <biancacatoto999@gmail.com>
2019-12-26 13:27:24 +0800

    updated landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -18,28 +18,54 @@ export default class Landing extends React.Component {
             <span className="title">Freat</span>
           </Container>
         </Container>
-        <Container fluid id="login-container" className="center p-0">
+        <Container fluid id="login-container" className="center">
           <Button variant="landing" size="lg" onClick={this.handleClick}>
             Log in through CAS
           </Button>
         </Container>
-        <Container fluid className="p-0">
+        <Container fluid>
           <Row className="center">
             <Col sm className="info-col center" >
+              <i className="fas fa-utensils"></i>
+              Be the first to find out about free food on campus!
+            </Col>
+            <Col sm className="info-col center">
               <i className="fas fa-map-marked-alt"></i>
-              <span className="center mt-2">
-                Find out about free food anywhere on campus!
-              </span>
+              Know where all free food is located in an instant!
             </Col>
             <Col sm className="info-col center">
               <i className="far fa-plus-square"></i>
-              <span className="center mt-2">
-                Have leftovers? Simply post a title, photo, and location to share with hungry students!
-              </span>
+              Have leftovers? Simply make a post and share your food with hungry students!
             </Col>
           </Row>
         </Container>
-        <Container fluid id="test" className="vh-100">
+        <Container fluid id="dev-container">
+          <Container fluid className="p-0">
+            <Row className="center">
+              <Col sm className="dev-col center" >
+                <span className="center">
+                  <strong>Bianca Catoto</strong><br/>
+                  BSE Computer Science '21
+                </span>
+              </Col>
+              <Col sm className="dev-col center">
+                <span className="center">
+                  <strong>Claire Dong</strong><br/>
+                  BSE Electrical Engineering '21
+                </span>
+              </Col>
+              <Col sm className="dev-col center">
+                <span className="center">
+                  <strong>Ibrahim Hashmi</strong><br/>
+                  BSE Electrical Engineering '21
+                </span>
+              </Col>
+              <Col sm className="dev-col center">
+                <strong>Gilron Tsabkevich</strong><br/>
+                BSE Computer Science '21
+              </Col>
+            </Row>
+          </Container>
         </Container>
       </>
     );
