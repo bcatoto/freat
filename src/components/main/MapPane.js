ea9388a0ae158c0c085b10c7b53c32e6a4170df3
Ibrahim-Hashmi <44486359+Ibrahim-Hashmi@users.noreply.github.com>
2019-12-30 02:47:39 -0500

    idk why it didn't push last time

--- src/components/main/MapPane.js
+++ src/components/main/MapPane.js
@@ -1,70 +1,59 @@
-import React, { useState, useEffect } from "react";
-import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import React, {Component} from 'react';
+import ReactMapGL, {NavigationControl, Marker,Popup} from 'react-map-gl';
+import { Icon } from 'semantic-ui-react';
 
 import coordinates from "../../assets/coordinates.json";
 
+const navStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  padding: '10px'
+};
+
 export default class MapPane extends React.Component {
   constructor(props) {
-      super(props);
-  }
+    super(props);
+    this.state = {
+      viewport: {
+        latitude: 40.346760,
+        longitude: -74.655187,
+        zoom: 16,
+        width: 1025,
+        height:700
+        }
+      };
+    }
 
-  renderMarkers() {
+    
 
-    const [selectedPark, setSelectedPark] = useState(null);
-
-    return this.props.posts.map(post => (
-      <>
-        <Marker
-          key={post.id}
-          latitude={coordinates[post.building][0]}
-          longitude={coordinates[post.building][1]}
-        >
-          <button
-            className="marker-btn"
-            onClick={e => {
-              e.preventDefault();
-              setSelectedPark(selectedPark);
-            }}
-          >
-          </button>
-        </Marker>
-        {selectedPark ? (
-          <Popup
-            latitude={coordinates[post.building][0]}
-            longitude={coordinates[post.building][0]}
-            onClose={() => setSelectedPark(null)}
-          >
-            <div>
-              <h3>{selectedPark.title}</h3> 
-            </div>
-          </Popup>
-        ) : null}
-      </>
-    ));
-  }
-
-  render() {
-    const [viewport, setViewport] = useState({
-      latitude: 40.346760,
-      longitude: -74.655187,
-      zoom: 16
-    });
+    render() {
+      const {viewport} = this.state;
 
     return (
-      <>
-        <ReactMapGL
-          {...viewport}
-          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-          mapStyle="mapbox://styles/mapbox/streets-v11"
-          onViewportChange={viewport => setViewport(viewport)}
+      <ReactMapGL
+        {...viewport}
+        onViewportChange={(viewport) => this.setState({viewport})}
+        mapStyle= "mapbox://styles/ibby/ck4s4lknb3ozs1crzpsxv5r4e"
+        mapboxApiAccessToken="pk.eyJ1IjoiaWJieSIsImEiOiJjazRpYm5sb3Ywa3UxM2VudGZsNmxrZDE2In0.v08PMm1hYXIQo6led-GbmQ" >
+        <div className="nav" style={navStyle}>
+          <NavigationControl onViewportChange={(viewport) => this.setState({viewport})}/>
+              {this.props.posts.map(post =>{
+                 return(
+                   <div key={post.id}> 
+                   <Marker  
+                      longitude={coordinates[post.building][1]} 
+                      latitude={coordinates[post.building][0]}>
+                      
+                   </Marker> 
+                   </div> 
+                  );
+                 }
+               ) }
+       </div>
+      </ReactMapGL>
 
-          loadingElement={<div style={{height : "100%"}} />}
-          containerElement={<div style={{height : "100%"}} />}
-          mapElement={<div style={{height : "100%"}} />}
-        >
-          {this.renderMarkers()}
-        </ReactMapGL>
-      </>
+      
     );
   }
 }
