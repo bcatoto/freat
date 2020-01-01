330ab127a953705feccd3c25a848ad58ed0bdef1
bcatoto <biancacatoto999@gmail.com>
2020-01-01 20:30:33 +0800

    minor fixes

--- src/components/main/MapPane.js
+++ src/components/main/MapPane.js
@@ -14,6 +14,7 @@ const navStyle = {
 export default class MapPane extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       viewport: {
         latitude: 40.346760,
@@ -21,12 +22,17 @@ export default class MapPane extends React.Component {
         zoom: 16,
         width: 1025,
         height:700
-        }
-      };
-    }
+      }
+    };
+  }
 
-    renderPopup(index){
-      return  (
+  renderPopup(index) {
+    const post = this.props.post;
+    if (post === undefined) {
+      return;
+    }
+    else {
+      return (
         <Popup tipSize={5}
           anchor="bottom-right"
           longitude={coordinates[post.building][1]}
@@ -35,36 +41,34 @@ export default class MapPane extends React.Component {
           closeOnClick={true}>
           <div>food</div>
         </Popup>
-        )
-      }
+      );
+    }
+  }
 
-    
-
-    render() {
-      const {viewport} = this.state;
+  render() {
+    const { viewport } = this.state;
 
     return (
       <ReactMapGL
         {...viewport}
-        onViewportChange={(viewport) => this.setState({viewport})}
+        onViewportChange={(viewport) => this.setState({ viewport })}
         mapStyle="mapbox://styles/ibby/ck4s4lknb3ozs1crzpsxv5r4e"
         mapboxApiAccessToken="pk.eyJ1IjoiaWJieSIsImEiOiJjazRpYm5sb3Ywa3UxM2VudGZsNmxrZDE2In0.v08PMm1hYXIQo6led-GbmQ" >
         <div className="nav" style={navStyle}>
-          <NavigationControl onViewportChange={(viewport) => this.setState({viewport})}/>
-              {this.props.posts.map((post, index) =>{
-                 return(
-                   <div key ={index}>
-                   <Marker  
-                      longitude={coordinates[post.building][1]} 
-                      latitude={coordinates[post.building][0]}>
-                      <Icon name='hospital' size='big' onMouseEnter={()=>this.setState({popupInfo: true})} onMouseLeave={()=>this.setState({popupInfo: null})}/>
-                   </Marker>  
-                   {this.renderPopup(index)}
-                   </div>
-                  );
-                 }
-               ) }
-       </div>
+          <NavigationControl onViewportChange={(viewport) => this.setState({ viewport })} />
+          {this.props.posts.map((post, index) => {
+            return (
+              <div key ={index}>
+                <Marker
+                  longitude={coordinates[post.building][1]}
+                  latitude={coordinates[post.building][0]}>
+                  <Icon name="hospital" size="big" onMouseEnter={() => this.setState({ popupInfo: true })} onMouseLeave={() => this.setState({ popupInfo: null })}/>
+                </Marker>
+                {this.renderPopup(index)}
+              </div>
+            );
+          })}
+        </div>
       </ReactMapGL>
     );
   }
