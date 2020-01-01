98ee777aab0cfbbdbb5dc053d6ca438eee6052cb
Ibrahim-Hashmi <44486359+Ibrahim-Hashmi@users.noreply.github.com>
2020-01-01 06:19:18 -0500

    new features added

--- src/components/main/MapPane.js
+++ src/components/main/MapPane.js
@@ -25,6 +25,19 @@ export default class MapPane extends React.Component {
       };
     }
 
+    renderPopup(index){
+      return  (
+        <Popup tipSize={5}
+          anchor="bottom-right"
+          longitude={coordinates[post.building][1]}
+          latitude={coordinates[post.building][0]}
+          onMouseLeave={() => this.setState({popupInfo: null})}
+          closeOnClick={true}>
+          <div>food</div>
+        </Popup>
+        )
+      }
+
     
 
     render() {
@@ -38,22 +51,21 @@ export default class MapPane extends React.Component {
         mapboxApiAccessToken="pk.eyJ1IjoiaWJieSIsImEiOiJjazRpYm5sb3Ywa3UxM2VudGZsNmxrZDE2In0.v08PMm1hYXIQo6led-GbmQ" >
         <div className="nav" style={navStyle}>
           <NavigationControl onViewportChange={(viewport) => this.setState({viewport})}/>
-              {this.props.posts.map(post =>{
+              {this.props.posts.map((post, index) =>{
                  return(
-                   <div key={post.id}> 
+                   <div key ={index}>
                    <Marker  
                       longitude={coordinates[post.building][1]} 
                       latitude={coordinates[post.building][0]}>
-                      
-                   </Marker> 
-                   </div> 
+                      <Icon name='hospital' size='big' onMouseEnter={()=>this.setState({popupInfo: true})} onMouseLeave={()=>this.setState({popupInfo: null})}/>
+                   </Marker>  
+                   {this.renderPopup(index)}
+                   </div>
                   );
                  }
                ) }
        </div>
       </ReactMapGL>
-
-      
     );
   }
 }
