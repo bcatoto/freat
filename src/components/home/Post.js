4a2db024a0da871d93fa4c50fdc225cac4c522b6
bcatoto <biancacatoto999@gmail.com>
2020-01-03 13:04:50 +0800

    fixed time

--- src/components/home/Post.js
+++ src/components/home/Post.js
@@ -77,14 +77,14 @@ export default class Post extends React.Component {
     const hour = min * 60;
     const tz = new Date().getTimezoneOffset() * min;
 
-    const now = Date.now();
+    const now = new Date().getTime()
     const time = new Date(this.props.post.created_at);
     const diff = now - time + tz;
 
     if (diff > 2 * hour) {
       return "2 hours ago"
     }
-    if (diff > hour) {
+    else if (diff > hour) {
       return "1h " + Math.floor((diff - hour) / min) + "m ago";
     }
     else if (diff > min){
